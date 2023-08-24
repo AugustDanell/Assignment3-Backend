e53ffe411fb983f2811743bf8fc00ebb035944f9
Vendela <vendela.o@hotmail.com>
2023-08-24 15:42:27 +0200

    pair programming, chore: swagger documentation for moviecontroller

---
+++ springweb/src/main/java/a3/springweb/springweb/controller/ApiErrorResponse.java
@@ -0,0 +1,5 @@
+package a3.springweb.springweb.controller;
+
+public class ApiErrorResponse {
+
+}
--- springweb/src/main/java/a3/springweb/springweb/controller/MovieController.java
+++ springweb/src/main/java/a3/springweb/springweb/controller/MovieController.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,6 +36,11 @@ import a3.springweb.springweb.model.entities.MovieCharacter;
 import a3.springweb.springweb.service.FranchiseService;
 import a3.springweb.springweb.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping(path = "api/v1/movies")
@@ -52,6 +58,12 @@ public class MovieController {
     }
 
     @Operation(summary = "Get all movies")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Success", content = {
+                @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = MovieDTO.class)))
+        }),
+        @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class)))
+})
     @GetMapping // GET: localhost:8080/api/v1/movies
     public ResponseEntity<Collection<MovieDTO>> getAll() {
         return ResponseEntity.ok(
@@ -59,6 +71,14 @@ public class MovieController {
                 movieService.findAll()));
     }
 
+    @Operation(summary = "Get a movie by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = MovieDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Movie does not exist with the given ID", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class)) }), 
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @GetMapping("{id}") // GET: localhost:8080/api/v1/movies/1
     public ResponseEntity<MovieDTO> getById(@PathVariable int id) {
         return ResponseEntity.ok(
@@ -66,6 +86,13 @@ public class MovieController {
                 movieService.findById(id)));
     }
 
+    @Operation(summary = "Add a movie")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Movie created", content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = MovieDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))),
+    })
     @PostMapping // POST: localhost:8080/api/v1/movies
     public ResponseEntity<MoviePostDTO> add(@RequestBody MoviePostDTO movieDto) {
         Movie mov = movieService.add(
@@ -75,7 +102,12 @@ public class MovieController {
         return ResponseEntity.created(location).build();
     }
 
-    
+    @Operation(summary = "Update a movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Movie successfully updated", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Movie not found with the given id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class))),
+    })
     @PutMapping("{id}")
     public ResponseEntity update(@RequestBody MovieUpdateDTO movieDto, @PathVariable int id) {
         // Validates if body is correct
@@ -87,6 +119,12 @@ public class MovieController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete a movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Movie successfully deleted"),
+            @ApiResponse(responseCode = "400", description = "Malformed request"),
+            @ApiResponse(responseCode = "404", description = "Movie not found with the given Id")
+    })
     @OnDelete(action= OnDeleteAction.CASCADE)
     @DeleteMapping("{id}") // DELETE: localhost:8080/api/v1/students/1
     public ResponseEntity<Movie> delete(@PathVariable int id) {
@@ -94,12 +132,26 @@ public class MovieController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Update characters in a movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Characters successfully updated", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorAttributeOptions.class)) }),
+            @ApiResponse(responseCode = "404", description = "Movie not found with the given ID", content = @Content)
+    })
     @PutMapping("characters/{id}")
     public ResponseEntity updateCharacters(@RequestBody int[] characterIds, @PathVariable int id) {
         movieService.updateCharacters(characterIds, id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get all characters in a franchise")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CharacterDTO.class))) }),
+            @ApiResponse(responseCode = "404", description = "Franchise does not exist with the given ID", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorResponse.class)) })
+    })
     @GetMapping("characters/{id}")
     public ResponseEntity<Collection<CharacterDTO>> getCharactersInFranchise(@PathVariable int id){
         return ResponseEntity.ok(
